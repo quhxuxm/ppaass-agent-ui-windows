8055ce57de6061f6e5ee21f268245e7a7742ecec
Qu Hao <quhxuxm_quh@163.com>
2024-03-25 16:05:01 +0800

    refact

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::sync::OnceLock;
-
 use clap::Parser;
 use ppaass_agent::config::AgentConfig;
-use ppaass_agent::error::AgentError;
 use ppaass_agent::server::AgentServer;
 use serde::{Deserialize, Serialize};
 use tauri::async_runtime::spawn;
-
-static AGENT_CONFIG: OnceLock<AgentConfig> = OnceLock::new();
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AgentUiInfo {
@@ -24,7 +19,7 @@ fn start_vpn(info: AgentUiInfo) {
     println!("Receive agent ui info: {:?}", info);
     spawn(async {
         let config = AgentConfig::parse();
-        match  AgentServer::new(config) {
+        match AgentServer::new(config) {
             Ok(server) => server.start(),
             Err(e) => {
                 println!("Fail to start agent server becuase of error: {e:?}")
@@ -39,7 +34,6 @@ fn stop_vpn() {
 }
 
 fn main() {
-    AGENT_CONFIG.get_or_init(|| AgentConfig::parse());
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![start_vpn, stop_vpn])
         .run(tauri::generate_context!())
