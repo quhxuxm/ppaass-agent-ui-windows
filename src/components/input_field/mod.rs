9831aa6c09f9cbdf36553a1961199e3c1a4d3aad
quhxuxm <quhxuxm.quh@gmail.com>
2024-04-18 21:53:24 +0800

    refact

--- src/components/input_field/mod.rs
+++ src/components/input_field/mod.rs
@@ -2,7 +2,7 @@ use std::cell::RefCell;
 
 use stylist::{yew::styled_component, StyleSource};
 use web_sys::HtmlInputElement;
-use yew::{classes, html, Callback, Classes, Html, NodeRef, Properties};
+use yew::{classes, html, use_state, Callback, Classes, Html, NodeRef, Properties};
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub enum InputFieldDataType {
@@ -31,11 +31,12 @@ pub struct InputFieldProps {
 
 #[styled_component(InputField)]
 pub fn input_field(props: &InputFieldProps) -> Html {
-    let input_state = match &props.value {
-        Some(value) => RefCell::new(value.clone()),
-        None => RefCell::new("".to_string()),
-    };
+    let is_error = use_state(|| false);
+    let value_state = use_state(|| props.value.clone());
+
     let on_change_callback = {
+        let is_error = is_error.clone();
+        let value_state = value_state.clone();
         let input_ref = props.input_ref.clone();
         let data_type = props.data_type;
         Callback::from(move |_| {
@@ -46,52 +47,49 @@ pub fn input_field(props: &InputFieldProps) -> Html {
             let input_value = html_input.value();
             if let InputFieldDataType::Number { min, max } = data_type {
                 if input_value.is_empty() {
+                    gloo::console::info!("Input empty value");
+                    is_error.set(false);
                     return;
                 }
                 match input_value.parse::<i128>() {
                     Ok(new_value) => {
-                        {
-                            let old_input_state = input_state.borrow();
-                            gloo::console::info!(
-                                "The old value: {}, the new value: {}",
-                                &old_input_state.to_string(),
-                                new_value
-                            );
-                            if min > new_value || max < new_value {
-                                html_input.set_value(&old_input_state);
-                                return;
-                            }
-                            html_input.set_value(new_value.to_string().as_str());
+                        gloo::console::info!("Input new value:", new_value);
+                        if min > new_value || max < new_value {
+                            value_state.set(Some(new_value.to_string()));
+                            is_error.set(true);
+                            return;
                         }
-                        input_state.replace(new_value.to_string());
+                        value_state.set(Some(new_value.to_string()));
+                        is_error.set(false);
                     }
-                    Err(e) => {
-                        let old_input_state = input_state.borrow();
+                    Err(_) => {
                         gloo::console::info!(
-                            "Recover to old value: {} because of error: {}",
-                            &old_input_state.to_string(),
-                            e.to_string()
+                            "Input new value but have error on parse to i128:",
+                            input_value.clone()
                         );
-                        html_input.set_value(&old_input_state);
+                        value_state.set(Some(input_value));
+                        is_error.set(true);
                     }
                 }
             }
         })
     };
+
     let style = StyleSource::try_from(include_str!("input_field.css")).unwrap();
     html! {
         <div class={classes!(style, props.classes.clone())}>
             if let Some(label) = &props.label {
-                <label for={props.id.clone()}>{label}</label>
+                <label for={props.id.clone()} class={classes!(is_error.then_some("error"))}>{label}</label>
             }
             <input id={props.id.clone()}
                 type="text" ref={&props.input_ref}
+                class={classes!(is_error.then_some("error"))}
                 placeholder={props.place_holder.clone()}
-                value={props.value.clone().unwrap_or("".to_string())}
-                oninput={on_change_callback}
+                value={(*value_state).clone()}
+                onchange={on_change_callback}
             />
             if let Some(hint) = &props.hint{
-                <span class="hint">{hint}</span>
+                <span class={classes!("hint", is_error.then_some("error"))}>{hint}</span>
             }
         </div>
 
