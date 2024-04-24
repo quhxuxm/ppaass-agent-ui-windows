c5aeeaffb3dabeaaa8dd880cb24012cebe1b1079
quhxuxm <quhxuxm.quh@gmail.com>
2024-04-24 20:54:44 +0800

    re-init

--- model/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod model;
-
-pub static AGENT_SERVER_EVENT: &str = "__AGENT_SERVER_EVENT__";
--- src/components/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod button;
-pub mod container;
-pub mod input_field;
-pub mod network_bar;
-pub mod network_chart;
